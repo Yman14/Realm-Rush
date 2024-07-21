85838b6c3223c55848d7d02fffcb378ae8cf1bc3
Yman <ymanneil07@gmail.com>
2024-07-21 20:44:39 +0800

    enemy update
    enemy ui, movement

--- Realm Rush/Assets/Scripts/EnemyMover.cs
+++ Realm Rush/Assets/Scripts/EnemyMover.cs
@@ -1,12 +1,13 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>();
-    [SerializeField] float enemyMovementSpeed = 1f;
+    [SerializeField] [Range(0f, 5f)] float enemyMovementSpeed = 1f;
 
     void Start()
     {
@@ -17,8 +18,20 @@ public class EnemyMover : MonoBehaviour
     {
         foreach (Waypoint waypoint in path)
         {
-            transform.position = waypoint.transform.position;
-            yield return new WaitForSeconds(enemyMovementSpeed);
+            Vector3 startPosition = transform.position;
+            Vector3 endPosition = waypoint.transform.position;
+            float travelPercent = 0f;
+
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1f)
+            {
+                travelPercent += enemyMovementSpeed * Time.deltaTime;
+                transform.position = Vector3.Lerp(startPosition, endPosition, travelPercent);
+                yield return null;
+            }
+
+            
         }
     }
 }
